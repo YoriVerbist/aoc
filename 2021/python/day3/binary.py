e18d2254a48cc4c6ad752191db8ab5bdb9c0c927
Yori Verbist <yori.verbist1212@gmail.com>
2021-12-04 10:57:16 +0100

    Day 3 - part 2 - Python

--- 2021/python/day3/binary.py
+++ 2021/python/day3/binary.py
@@ -1,4 +1,4 @@
-def calculate_rate(kind, lines):
+def calculate_rate(kind: str, lines: list) -> int:
     sum = {}
     for i in range(len(lines[0])):
         sum[i] = 0
@@ -13,9 +13,7 @@ def calculate_rate(kind, lines):
                 gamma_rate += "1"
             else:
                 gamma_rate += "0"
-        print(gamma_rate)
         gamma_rate = int(gamma_rate, 2)
-        print(gamma_rate)
         return gamma_rate
     if kind == "epsilon":
         for i in range(len(lines)):
@@ -32,6 +30,46 @@ def calculate_rate(kind, lines):
         return epsilon_rate
 
 
+def calculate_most_common(position: int, lines: list) -> list:
+    if len(lines) == 1:
+        return lines[0]
+    else:
+        sum = 0
+        for i in range(len(lines)):
+            if lines[i][position] == "1":
+                sum += 1
+        temp = []
+        if sum >= (len(lines) / 2):
+            for line in lines:
+                if line[position] == "1":
+                    temp.append(line)
+        else:
+            for line in lines:
+                if line[position] == "0":
+                    temp.append(line)
+        return calculate_most_common(position + 1, temp)
+
+
+def calculate_least_common(position: int, lines: list) -> list:
+    if len(lines) == 1:
+        return lines[0]
+    else:
+        sum = 0
+        for i in range(len(lines)):
+            if lines[i][position] == "1":
+                sum += 1
+        temp = []
+        if sum >= (len(lines) / 2):
+            for line in lines:
+                if line[position] == "0":
+                    temp.append(line)
+        else:
+            for line in lines:
+                if line[position] == "1":
+                    temp.append(line)
+        return calculate_least_common(position + 1, temp)
+
+
 def part1():
     with open("input.txt", "r") as file:
         lines = [line.rstrip() for line in file.readlines()]
@@ -42,8 +80,15 @@ def part1():
         print(f"Power consumption of the submarine is: {consumption}")
 
 
-part2():
-    with open("input.txt", 'r') as file:
-        lines = line.rstrip() for line in file.realines()]
+def part2():
+    with open("input.txt", "r") as file:
+        lines = [line.rstrip() for line in file.readlines()]
+        most_common = int(calculate_most_common(0, lines), 2)
+        least_common = int(calculate_least_common(0, lines), 2)
+        print(most_common, least_common)
+        print(
+            f"The life support rating of the submarine is: {most_common * least_common}"
+        )
+
 
 part2()
