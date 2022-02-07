133aec46069c1acd2cc4153947e3dbb75bf08dd6
Yori Verbist <yori.verbist1212@gmail.com>
2022-02-07 13:07:15 +0100

    Day 4 - part 1 finished

--- 2021/python/day4/bingo.py
+++ 2021/python/day4/bingo.py
@@ -1,79 +1,91 @@
 import numpy as np
 
 
-def initialize_empty_boards(boards: list) -> np.ndarray:
+def initialize_empty_boards(boards):
     return np.zeros_like(boards)
 
 
-def check_winner(boards: list) -> int:
-    boards = boards.tolist()
-    for i in range(len(boards)):
-        # Check for horizontal winners
-        for row in boards[i]:
-            if row.count(1) == len(row) and row[0] != None:
-                return i
-        # Check for vertical winners
-        for col in range(len(boards[i][0])):
-            check = []
-            for row in boards[i]:
-                check.append(row[col])
-            if check.count(1) == len(check) and check[0] != None:
-                return i
-    return -1
-
-
-def check_if_num_in_board(num: int, board: list) -> list:
-    for i in range(len(board)):
-        for j in range(len(board[i])):
-            if board[i][j] == num:
-                return [i, j]
-    return []
-
-
-def play_bingo(draws: list, boards: list) -> list:
+def play_bingo(numbers, boards):
     empty_boards = initialize_empty_boards(boards)
-    for num in draws:
-        for i in range(len(boards)):
-            in_board = check_if_num_in_board(num, boards[i])
-            if in_board != []:
-                empty_boards[i][in_board[0]][in_board[1]] = 1
-        board_nr = check_winner(empty_boards)
-        if board_nr != -1:
-            return board_nr, empty_boards, num
+    for num in numbers:
+        positions = check_num_in_board(num, boards)
+        if positions == set():
+            continue
+        for i, j, k in positions:
+            empty_boards[i][j][k] = 1
+            winners = check_winner(empty_boards)
+            if winners != set():
+                winners = list(winners)
+                sum = calculate_unmarked_numbers(
+                    boards[winners[0]], empty_boards[winners[0]]
+                )
+                return winners, sum, num
 
 
-def calculate_non_marked(board, empty_board):
+def calculate_unmarked_numbers(board, marker_board):
     sum = 0
-    for i in range(len(board)):
-        for j in range(len(board[i])):
-            if empty_board[i][j] == 0:
-                sum += board[i][j]
+    for i, row in enumerate(board):
+        for j, num in enumerate(row):
+            if marker_board[i][j] == 0:
+                sum += num
     return sum
 
 
-def part1():
-    with open("input.txt", "r") as file:
-        lines = [line.rstrip() for line in file.readlines()]
-        draws = [int(num) for num in lines[0].split(",")]
-        boards = [[]]
-        lines = lines[2:]
-        pos = 0
+def check_winner(boards):
+    winners = set()
+    # Check for horizontal winners
+    for i, board in enumerate(boards):
+        for j, row in enumerate(board):
+            if np.count_nonzero(row) == len(row):
+                winners.add(i)
+    # Check for vertical winners
+    for i, board in enumerate(boards):
+        temp = np.transpose(board)
+        for j, row in enumerate(temp):
+            if np.count_nonzero(row) == len(row):
+                winners.add(i)
+    return winners
+
+
+def check_num_in_board(num, boards):
+    pos = set()
+    for i, board in enumerate(boards):
+        for j, row in enumerate(board):
+            for k, number in enumerate(row):
+                if number == num:
+                    pos.add((i, j, k))
+    return pos
+
+
+def get_input(filename):
+    with open(filename, "r") as f:
+        lines = [line.strip("\n") for line in f.readlines() if line != "\n"]
+        numbers = [int(num) for num in lines[0].split(",")]
+        boards = list()
+        lines = lines[1:]
+        pos = -1
         for i in range(len(lines)):
-            if lines[i] == "":
-                boards.append([])
+            row = lines[i].split(" ")
+            temp = list()
+            for num in row:
+                if num == "":
+                    continue
+                temp.append(int(num))
+            if i % 5 == 0:
                 pos += 1
-            else:
-                temp = []
-                for num in lines[i].split(" "):
-                    if num == "":
-                        continue
-                    temp.append(int(num))
-                boards[pos].append(temp)
+                boards.append([])
+            boards[pos].append(temp)
 
         boards = np.array(boards)
-        board_nr, empty_boards, num = play_bingo(draws, boards)
-        total = calculate_non_marked(boards[board_nr], empty_boards[board_nr])
-        print(f"Final score is: {total * num}")
+        return numbers, boards
 
 
-part1()
+def part_1():
+    filename = "input.txt"
+    numbers, boards = get_input(filename)
+    winner, sum, num = play_bingo(numbers, boards)
+    print(winner, sum, num)
+    print(f"solution is: {sum * num}")
+
+
+part_1()
