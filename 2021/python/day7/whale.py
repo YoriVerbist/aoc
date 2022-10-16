7cf29579d93edf75e581dfae2f89ed46f4ddf4e2
Yori Verbist <yori.verbist1212@gmail.com>
2022-10-16 18:59:11 +0200

    Changes

--- 2021/python/day7/whale.py
+++ 2021/python/day7/whale.py
@@ -5,7 +5,7 @@ def part_1():
     with open("input.txt") as file:
         line = file.readline().strip("\n")
         positions = [int(num) for num in line.split(",")]
-        fuel, position = find_cheapest_position(positions, "normal")
+        fuel = find_cheapest_position(positions, "normal")
         print(fuel)
 
 
@@ -13,7 +13,7 @@ def part_2():
     with open("test.txt") as file:
         line = file.readline().strip("\n")
         positions = [int(num) for num in line.split(",")]
-        fuel, position = find_cheapest_position(positions, "stacking")
+        fuel = find_cheapest_position(positions, "stacking")
         print(fuel)
 
 
@@ -21,7 +21,6 @@ def find_cheapest_position(positions, counting_type):
     min_pos = min(positions)
     max_pos = max(positions)
     current_pos = sum(positions) // len(positions)
-    print("cp", current_pos)
     possible_possitions = {i: math.inf for i in range(min_pos, max_pos + 1)}
     while True:
         total = 0
@@ -30,9 +29,7 @@ def find_cheapest_position(positions, counting_type):
                 total += abs(current_pos - value)
         if counting_type == "stacking":
             for value in positions:
-                # temp = accumulating_increase(current_pos, value)
-                temp = sum(range(abs(current_pos - value)))
-                total += temp
+                total += sum([x for x in range(abs(current_pos - value) + 1)])
         possible_possitions[current_pos] = total
         if (
             possible_possitions[current_pos] < possible_possitions[current_pos + 1]
@@ -40,21 +37,11 @@ def find_cheapest_position(positions, counting_type):
             and possible_possitions[current_pos + 1] != math.inf
             and possible_possitions[current_pos - 1] != math.inf
         ):
-            return total, current_pos
+            return total
         elif possible_possitions[current_pos] > possible_possitions[current_pos + 1]:
             current_pos += 1
         else:
             current_pos -= 1
-        print("cp:", current_pos)
-
-
-def accumulating_increase(low, high):
-    difference = abs(high - low)
-    total = 0
-    for i in range(difference + 1):
-        total += i
-    print(total)
-    return total
 
 
 part_1()
