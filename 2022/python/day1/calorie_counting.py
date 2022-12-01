0e67420a7ad39072acc772e06e6c8d572c58e094
Yori Verbist <yori.verbist1212@gmail.com>
2022-12-01 20:08:20 +0100

    code clean-up

--- 2022/python/day1/calorie_counting.py
+++ 2022/python/day1/calorie_counting.py
@@ -1,28 +1,10 @@
-def part_1(cals):
-    print(max(cals.values()))
-
-
-def part_2(cals):
-    cals = sorted(cals.values(), reverse=True)
-    print(sum(cals[0:3]))
-
-
-def parse_data(l):
-    count = 0
-    cals = {}
-    cals[count] = 0
-    for line in l:
-        if line != "\n":
-            cals[count] += int(line.strip("\n"))
-        else:
-            count += 1
-            cals[count] = 0
-    return cals
-
-
 if __name__ == "__main__":
     with open("input.txt") as file:
-        lines = file.readlines()
-        calories = parse_data(lines)
-        part_1(calories)
-        part_2(calories)
+        calories = list(
+            map(
+                lambda x: sum(map(int, x.strip().split("\n"))),
+                file.read().split("\n\n"),
+            )
+        )
+        print("part 1:", max(calories))
+        print("part 2:", sum(sorted(calories, reverse=True)[0:3]))
